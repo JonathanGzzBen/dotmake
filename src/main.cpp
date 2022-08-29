1f4cf6105f39fafa502132e274a9dfc8f2db1a89
JonathanGzzBen <jonathangzzben@gmail.com>
2022-08-29 14:01:36 -0500

    Add clang-format and fix main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,19 @@
-#include <iostream>
 #include <string.h>
+
+#include <iostream>
 #include <thread>
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   if (argc == 1) {
     std::cout << "usage: dotmake [-h | --help] FILE... TASK\n";
     return 0;
   }
   for (int i{0}; i < argc; i++) {
-    if(strncmp(argv[i], "--help", 6) == 0) {
-      std::cout << "Print help\n";
+    if (strncmp(argv[i], "--help", 6) == 0) {
+      std::cout << "usage: dotmake [-h | --help] FILE... TASK\n";
       return 0;
     }
-    if(strncmp(argv[i], "-h", 2) == 0) {
+    if (strncmp(argv[i], "-h", 2) == 0) {
       std::cout << "Print help\n";
       return 0;
     }
