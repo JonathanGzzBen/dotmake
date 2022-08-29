08df4d7ac2ce7f126e6b33ed0341e672c51de162
JonathanGzzBen <jonathangzzben@gmail.com>
2022-08-28 22:31:13 -0500

    Add mock help menu

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
+#include <string.h>
 #include <thread>
 
-int main() {
-  std::cout << "Hello\n";
+int main(int argc, char* argv[]) {
+  if (argc == 1) {
+    std::cout << "usage: dotmake [-h | --help] FILE... TASK\n";
+    return 0;
+  }
+  for (int i{0}; i < argc; i++) {
+    if(strncmp(argv[i], "--help", 6) == 0) {
+      std::cout << "Print help\n";
+      return 0;
+    }
+    if(strncmp(argv[i], "-h", 2) == 0) {
+      std::cout << "Print help\n";
+      return 0;
+    }
+  }
   return 0;
 }
